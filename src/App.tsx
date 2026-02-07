3fade92561ba6fbab46e5867fcd870f246c4ac52
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-02-07 19:30:07 +0900

    ⚡ Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+
+type Pokemon = {
+  id: number;
+  name: string;
+  image: string;
+};
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then(res => res.json())
+      .then(setList);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>配布ポケモン一覧</h1>
+      <ul>
+        {list.map(p => (
+          <li key={p.id}>
+            <img src={p.image} alt="" />
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 export default App
